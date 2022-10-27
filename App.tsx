6727434b5a8ecfe6de55d227cd0465730a414cf3
parK-dev <kirk.paradis@gmail.com>
2022-10-27 09:45:50 +0900

    Barebones Adjust example

--- App.tsx
+++ App.tsx
@@ -1,7 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Adjust, AdjustConfig } from "react-native-adjust";
 
 export default function App() {
+  useEffect(() => {
+    const adjustConfig = new AdjustConfig(
+      "2fm9gkqubvpc",
+      AdjustConfig.EnvironmentSandbox
+    );
+    adjustConfig.setLogLevel(AdjustConfig.LogLevelVerbose);
+    Adjust.create(adjustConfig);
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
